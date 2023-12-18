31e15bee8e4fe3d9034e357bea20ecb7ec445169
spences10 <spences10apps@gmail.com>
2023-12-18 07:49:25 +0000

    refactor: :recycle: vite optimise deps

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	optimizeDeps: {
+		exclude: ['@libsql/client'],
+	},
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+	},
 });
